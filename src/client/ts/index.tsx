4d06b8f58eec6bfe24fcc77a543bebd4f1d47220
Joshua Kitchen <joshiemoto@gmail.com>
2022-05-20 20:49:32 +0100

    modified font to Roboto Slab

--- src/client/ts/index.tsx
+++ src/client/ts/index.tsx
@@ -60,10 +60,12 @@ const HomePage = function () {
       <p>
         You may have noticed this looks unbelievably simple, the fact it's got
         TypeScript, React, SASS, a node backend and is dockerised should tell
-        you exactly the kind of nutjob you're dealing with here. This will not
-        be an organised website but a cacophany of chaos creating project after
-        project after project which will ultimately result in none of them being
-        finished and all of them being a little bit shit.
+        you exactly the kind of nutjob you're dealing with here.
+      </p>
+      <p>
+        This will not be an organised website but a cacophany of chaos creating
+        project after project after project which will ultimately result in none
+        of them being finished and all of them being a little bit shit.
       </p>
     </div>
   )
